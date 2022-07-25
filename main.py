f57fba7887225f0476bcdd3fdddadfe344a89c84
Jonasz Klimala <jonaszklimala@interia.pl>
2022-07-25 12:20:44 +0200

    files are renamed to .jpg, so you can easily open them

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from pathlib import Path
-import os
 
 
 def copy():
     source_path = Path.joinpath(Path.home(), "AppData", "Local", "Packages",
                                 "Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy", "LocalState", "Assets")
-    print(Path.home())
     target_path = Path().cwd()
-    file_pattern = "*.*"
+    new_suffix = ".jpg"
     for obj in source_path.iterdir():
         if obj.is_file():
-            obj.rename(target_path / obj.name)
+            new_name = f"{obj.stem}{new_suffix}"
+            print(new_name)
+            obj.rename(target_path / new_name)
 
 
 copy()
