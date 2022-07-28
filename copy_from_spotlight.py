cc7afc273824000b36be503098dba7ff045ed40b
Jonasz Klimala <jonaszklimala@interia.pl>
2022-07-28 12:56:54 +0200

    exception caching and while loop added,

--- copy_from_spotlight.py
+++ copy_from_spotlight.py
@@ -11,14 +11,20 @@ def copy():
     new_suffix = ".jpg"
     input("Files will be copied from source directory to 'img' directory, created in place where script was started. "
           "Press any button to continue.")
-    copytree(source_path, target_path, dirs_exist_ok=True)  # copy first so original files remain intact
-    for i, obj in enumerate(target_path.iterdir()):
-        if obj.is_file():
-            new_name = f"img_{i}{new_suffix}"
-            print(new_name, " created")
-            obj.rename(target_path / new_name)
-    input("Now you can select images you like, move them wherever you want and delete everything from 'img "
-          "folder. Press any button to end.")
+    loop = True
+    while loop:  # while loop so we can easily restart after exception
+        try:
+            copytree(source_path, target_path, dirs_exist_ok=True)  # copy first so original files remain intact
+            for i, obj in enumerate(target_path.iterdir()):
+                if obj.is_file():
+                    new_name = f"img_{i}{new_suffix}"
+                    obj.rename(target_path / new_name)
+                    print(new_name, " created")
+            input("Now you can select images you like, move them wherever you want and delete everything from 'img' "
+                  "folder. Press any button to end.")
+            loop = False
+        except FileExistsError:
+            input("Please, delete all files from 'img' folder. Press any button to continue.")
 
 
 copy()
