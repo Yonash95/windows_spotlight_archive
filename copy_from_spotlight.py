e1e9d0944bf8fdfe2f06084f3fd295a4ed44f188
Jonasz Klimala <jonaszklimala@interia.pl>
2022-07-28 12:47:18 +0200

    instructions in 'input' added

--- copy_from_spotlight.py
+++ copy_from_spotlight.py
@@ -9,12 +9,16 @@ def copy():
                                 "Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy", "LocalState", "Assets")
     target_path = Path.joinpath(Path().cwd(), 'img')
     new_suffix = ".jpg"
+    input("Files will be copied from source directory to 'img' directory, created in place where script was started. "
+          "Press any button to continue.")
     copytree(source_path, target_path, dirs_exist_ok=True)  # copy first so original files remain intact
     for i, obj in enumerate(target_path.iterdir()):
         if obj.is_file():
             new_name = f"img_{i}{new_suffix}"
-            print(new_name)
+            print(new_name, " created")
             obj.rename(target_path / new_name)
+    input("Now you can select images you like, move them wherever you want and delete everything from 'img "
+          "folder. Press any button to end.")
 
 
 copy()
